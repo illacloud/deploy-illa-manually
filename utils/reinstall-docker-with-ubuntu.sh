24a357b02c995bf51807a191610739deb9bfb861
karminski <code.karminski@outlook.com>
2023-01-05 15:16:36 +0800

    ADD utils scripts.

---
+++ utils/reinstall-docker-with-ubuntu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo ''
+echo 'Removing old docker installation.'
+echo ''
+
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
+echo ''
+echo 'Do update and add ubuntu official install source.'
+echo ''
+
+sudo apt-get update 
+ sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+echo ''
+echo 'installing docker-ce.'
+echo ''
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
